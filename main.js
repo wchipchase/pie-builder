a2861b523870853fade21713e8be4f2d65922cb8
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-03-26 00:00:32 -0600

    delete button commit

--- main.js
+++ main.js
@@ -2,10 +2,30 @@ const addButton = document.getElementById('addButton');
 const inputIngredient = document.getElementById('inputIngredient');
 
 const ingredients = [];
+let ingredientCounter = 1;
 
 const printToDom = (divId, textToPrint) => {
     let selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
+};
+
+const deleteFunction = (e) => {
+    const buttonId = e.target.id;
+    ingredients.forEach((ingredient, index) => {
+        if(ingredient.id === buttonId) {
+            // console.log('this one', ingredient)
+            ingredients.splice(index, 1);
+        }
+    })
+    domStringBuilder(ingredients);
+    addDeleteEvents();
+}
+
+const addDeleteEvents = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+    for(let i=0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', deleteFunction);
+    }
 };
 
 const domStringBuilder = (arrayToPrint) => {
@@ -14,7 +34,7 @@ const domStringBuilder = (arrayToPrint) => {
       domString += `<div class="card col-3">`;
       domString += `  <div class="card-body">`;
       domString += `    <h5 class="card-title">${ingredient.item}</h5>`;
-      // domString += `    <a href="#" class="btn btn-primary">Go somewhere</a>`;
+      domString += `    <class="btn btn-danger deleteButton" id=${ingredient.id}>Delete</a>`;
       domString += `  </div>`;
       domString += `</div>`;
     });
@@ -26,11 +46,14 @@ const addIngredient = (e) => {
     e.preventDefault();
     const inputText = inputIngredient.value;
     const newIngredient = {
-        item: inputText
+        item: inputText,
+        id: `ingredient${ingredientCounter}`
     }
-    ingredients.push(newIngredient)
+    ingredients.push(newIngredient);
+    ingredientCounter ++;
     printToDom("ingredient-container", inputText);
-    domStringBuilder(ingredients)
+    domStringBuilder(ingredients);
+    addDeleteEvents();
     inputIngredient.value='';
 }
 
